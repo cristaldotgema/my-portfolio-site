dd5ad8cda785361462ebe5f5c2c66296653f37e5
Magz <cyanjupiter@gmail.com>
2018-10-10 11:51:30 -0400

    all base pages done, 90% finished (one has bad css because i'm working on it). parallax page is included but not optimized yet

---
+++ dist/js/mailerlite.js
@@ -0,0 +1,29 @@
+//mailerlight universal
+(function(m, a, i, l, e, r) {
+  m['MailerLiteObject'] = e;
+  function f() {
+    var c = { a: arguments, q: [] };
+    var r = this.push(c);
+    return 'number' != typeof r ? r : f.bind(c.q);
+  }
+  f.q = f.q || [];
+  m[e] = m[e] || f.bind(f.q);
+  m[e].q = m[e].q || f.q;
+  r = a.createElement(i);
+  var _ = a.getElementsByTagName(i)[0];
+  r.async = 1;
+  r.src = l + '?v' + ~~(new Date().getTime() / 1000000);
+  _.parentNode.insertBefore(r, _);
+})(
+  window,
+  document,
+  'script',
+  'https://static.mailerlite.com/js/universal.js',
+  'ml'
+);
+
+var ml_account = ml('accounts', '1239056', 'e8q7i7k3x5', 'load');
+//mailerlight universal
+
+var ml_webform_1000312 = ml_account('webforms', '1000312', 'v8e6o7', 'load');
+ml_webform_1000312('animation', 'fadeIn');
